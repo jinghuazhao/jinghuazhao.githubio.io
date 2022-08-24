8c79b22b7a5f72d03ff51f410a2da68e7d8e80e3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-08-24 15:55:37 +0100

    _site

---
+++ docs/_site/tools/docs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+cd docs
+bundle exec jekyll build
+cd -
+for f in .gitignore $(ls)
+do
+  git add ${f}
+  git commit -m "${f}" --no-verify
+done
+git push
+du -hs --exclude .git --exclude _site
