583bdc8c70611927a3ac323751c1685ae6ff0056
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-06-23 10:18:57 +0100

    docs

--- docs/tools/docs.sh
+++ docs/tools/docs.sh
@@ -13,6 +13,13 @@ function gh_pages()
 cd docs
 bundle exec jekyll build
 cd -
+
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   echo icelake
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+fi
+
 for f in .gitignore $(ls)
 do
   git add ${f}
